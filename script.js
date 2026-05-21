a078203512a87d693d4534799183b07c68f5de64
Elisa Mujica <elisa.m.mujica@gmail.com>
2026-05-21 14:49:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle('active');
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+    console.log('page loaded');
+
+    document.documentElement.style.scrollBehavior = 'smooth';
+    
+    const visitBtn = document.querySelector('.visit-btn');
+    visitBtn.addEventListener('click', () => {
+        window.open(visitBtn.dataset.link,'_blank','noopener');
+    });
+});
+
+document.addEventListener('DOMContentLoaded',function(){
+
+    const socialIcons = document.querySelectorAll('.socials i');
+    socialIcons.forEach(icon => {
+        icon.addEventListener('click' ,() => {
+            window.open(icon.dataset.link,'_blank','noopener');
+        });    
+    });
+});
